973324a245e1193e8c9443cf7e44151edaa5d456
Priyanka <priyanka3007@gmail.com>
2017-01-09 14:28:25 -0800

    basic exersices

---
+++ exercise1.rb
@@ -0,0 +1,54 @@
+#comment:single line
+=begin 
+
+one comment
+second comment
+thir comment
+
+=end
+
+
+# puts "Hello"
+# puts "Coding"
+# puts "Dojo"
+
+# print " Hi "
+# print "Pryanka"
+
+# BEGIN{
+# 	puts "I am in Begin block"
+# }
+
+# END{
+# 	puts "I am in End block"
+# }
+
+# x=2
+# y=9
+# puts z=x+y
+
+# x=2;y=23; puts z=x+y
+
+first_name="Priyanka"
+last_name="Shah"
+# puts first_name
+# puts last_name
+
+# puts first_name, last_name
+
+# puts first_name+ " " +last_name
+
+# puts "My first name is #{first_name} and my last name is #{last_name}" 
+
+# puts "My first name is %s and my last name is %s" %[first_name, last_name]
+
+# z=3
+# puts "Print out %d" %z
+
+puts "\t\tI am\n 5'8\" tall"
+
+
+
+
+
+
