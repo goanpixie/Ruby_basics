49958fb4f1cfa331cbe6dece8816bea22ee0bdde
Priyanka <priyanka3007@gmail.com>
2017-01-09 15:31:35 -0800

    loops

--- exercise4_loops.rb
+++ exercise4_loops.rb
@@ -1,7 +1,18 @@
-i=5
-num=5
-while i<num
-	puts "inside the loop i equals #{i}"
-	i++
+# i=1
+# num=5
+# while i<num
+# 	puts "inside the loop i = #{i}"
+# 	i+=1
+# end
+
+# i = 0
+# num = 5
+# begin
+#    puts "Inside the loop i = #{i}"
+#    i +=1
+# end while i< num
+
+for i in 0..5
+	puts "i in local variable is #{i}"
 end
 
