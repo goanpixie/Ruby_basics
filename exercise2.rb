b561c7dc203f1095ee0abf4e6a9aa0f08312c3b3
Priyanka <priyanka3007@gmail.com>
2017-01-09 14:55:43 -0800

    str fnx

---
+++ exercise2.rb
@@ -0,0 +1,30 @@
+x="Jumping Monkeys"
+y=""
+
+# puts x
+# puts x.length
+# puts x.capitalize
+# puts x.downcase
+# puts x.upcase
+# puts x.include?("monkeys")
+# puts x.include?("Monkeys")
+# puts "I will print this" if x.include?("Monkeys")
+# puts x[3]
+
+# p= "Trump, Pence, Assholes"
+
+# puts "String is empty" if y.empty?
+
+# puts p.split(",").to_s
+
+# puts '24 plus 8 is #{24 + 8}' 
+
+# puts "24 plus 8 is #{24 + 8}"
+
+puts "Chunky" << "Bacon" 
+
+puts "Chunky"[2..3]
+
+
+
+
