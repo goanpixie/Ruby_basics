2d7d28b6437e508788db7ce5863a239892f35672
Priyanka <priyanka3007@gmail.com>
2017-01-09 23:02:48 -0800

    ranges

---
+++ exercise7_range.rb
@@ -0,0 +1,15 @@
+x = (1..7)
+
+# puts "Class name #{x.class}"
+
+# puts "its does include 3!" if x.include? 3
+
+# puts "Last number on this range is"+ x.last.to_s
+
+# puts "Max number on this range is"+ x.max.to_s
+
+# puts "Min number on this range is"+ x.min.to_s
+
+y = ("a".."z")
+# puts y.to_a.shuffle.to_s
+puts y.include?("g") 
