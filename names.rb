2852202479968bd357fb3a771556ea59e2a7cd13
Priyanka <priyanka3007@gmail.com>
2017-01-10 18:12:22 -0800

    Completed

--- names.rb
+++ names.rb
@@ -18,20 +18,47 @@
 # puts newArr.to_s
 
 
-x = ("a".."z")
-shuffle = (x.to_a.shuffle)
-puts shuffle.to_s
-puts shuffle.first
-puts shuffle.last
-vowels=["i","e","o","u","a"]
-	for i in vowels
-		if i == shuffle.first
-			puts "I am a vowel"
-		end
-	end
+# x = ("a".."z")
+# shuffle = (x.to_a.shuffle)
+# puts shuffle.to_s
+# puts shuffle.first
+# puts shuffle.last
+# vowels=["i","e","o","u","a"]
+# 	for i in vowels
+# 		if i == shuffle.first
+# 			puts "I am a vowel"
+# 		end
+# 	end
 
-x = (55..100)
+
+# x = []
+# 10.times{|i| x.push(i=rand(55..100))}
+# puts x.sort
+# puts x.min
+# puts x.max
+
+# a = []
+# (1..5).each {|i| a.push((65+rand(26)).chr) }
+# puts a.join
+
+# x = []
+# 10.times{|i| x.push((1..5).collect {|i| i = (65+rand(26)).chr}.join)}
+# puts x
+
+
+a = {:first_name => "Michael", :last_name => "Choi"}
+b = {:first_name => "John", :last_name => "Supsupin"}
+c = {:first_name => "KB", :last_name => "Tonel"}
+d = {:first_name => "Jessie", :last_name => "De Leon"}
+e = {:first_name => "Jaybee", :last_name => "Balog"}
+names = [a, b, c, d, e]
+length =names.length
+    puts "There are " + names.count.to_s + " names in the 'names' array"
+
+names.each{|names| puts "the name is " +names.values.join(" ")}
 	
+
+
 
 
 
