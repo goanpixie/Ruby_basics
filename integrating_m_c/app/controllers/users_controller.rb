cbae0029125ae4d461d257e4879dfb173cd9ac50
Priyanka <priyanka3007@gmail.com>
2017-01-20 18:15:41 -0800

    Semi completed

---
+++ integrating_m_c/app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def index
+  	@users = User.all
+  	render json: @users
+  end
+
+  def create
+  	@user = User.create(name: params[:name])
+  	redirect_to '/users/index'
+  end
+end
