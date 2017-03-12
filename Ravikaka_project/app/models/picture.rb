487ef655de14fa5ee12d4e796824c166e7f1068e
Priyanka <priyanka3007@gmail.com>
2017-03-11 18:43:00 -0800

    paprclip addition

--- Ravikaka_project/app/models/picture.rb
+++ Ravikaka_project/app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ActiveRecord::Base
   belongs_to :user
-  # has_attached_file :image
+  has_attached_file :image
   # validates_attachment :image, presence: true,
   # 					   :content_type => { :content_type => "image/jpg" }
   # do_not_validate_attachment_file_type :image
